293c458cb093d683fc12173a6d4890ee7f6aeab5
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2024-02-04 18:27:50 +0530

    s

--- my-app/src/components/Todo.jsx
+++ my-app/src/components/Todo.jsx
@@ -5,20 +5,38 @@ import { useState } from "react";
 function Todo() {
 
     const [ text, setText ]= useState("")
+    const [todos, setTodos] = useState([])
 
     const handleChange = (e) =>{
         setText(e.target.value)
     }
 
     const handleAdd = () =>{
-        console.log("Button Clicked")
+        
+        const newitem = {
+            id: Date.now() + Math.random() + text,
+            title:text,
+            status:false
+        }
+
+        const todolistafterAddingitem = [...todos,newitem]
+
+
+        setTodos(todolistafterAddingitem)
+        setText("")
     }
+    console.log(todos)
 
     return <div>
         <h1>sahil Todo app</h1>
         <div>
             <input placeholder="Add a todo " value={text} onChange={handleChange} />
             <button onClick={handleAdd}>Add Todo</button>
+        </div>
+        <div>
+            {todos.map((todo)=>
+            <div key= {todo.id}>{todo.title}-{todo.status ? "completed" : "not completed"}</div>
+            )}
         </div>
     </div>
 }
