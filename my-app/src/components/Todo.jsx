6899002a0ba95c992566a1b60791c4373cb16665
Sahil18718 <119488054+Sahil18718@users.noreply.github.com>
2024-02-06 13:52:43 +0530

    m

--- my-app/src/components/Todo.jsx
+++ my-app/src/components/Todo.jsx
@@ -57,3 +57,5 @@ function Todo() {
 }
 
 export default Todo;
+
+
---
+++ react-api-app/src/App.jsx
@@ -0,0 +1,42 @@
+// src/App.js
+import React, { useEffect, useState, useRef, useMemo } from 'react';
+import useApi from './useApi';
+
+
+const App = () => {
+  // taking input
+  const [inputValue, setInputValue] = useState('');
+  const inputRef = useRef();
+
+  const apiUrl = useMemo(() => `https://jsonplaceholder.typicode.com/todos/${inputValue}`, [inputValue]);
+
+  // taking props from other file and working on this page
+  const { data, loading, error } = useApi(apiUrl);
+
+  useEffect(() => {
+    // You can perform additional actions after data is fetched
+    // For example, focus on an input field after data is loaded
+    inputRef.current.focus();
+  }, [data]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  return (
+    <div>
+      <h1>React API App</h1>
+      <input type="text" value={inputValue} onChange={handleInputChange} ref={inputRef} />
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      {data && (
+        <div>
+          <h2>Data:</h2>
+          <pre>{JSON.stringify(data, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
